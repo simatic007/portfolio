6ca96011eef8ccd4a91e2193c955a49ce0c384e5
Anton Chubenka <simatic007@tut.by>
2017-05-31 15:59:09 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by simatic on 12.04.17.
+ */
+
+$(function(){
+    $('.carousel').carousel({
+        interval: 500000
+    });
+    /*
+    $(function(){       
+        $('#form').validator();        
+    });*/
+});
